348d4cf851d152ab8916b02cce61c57bfde25573
SalkovAA <lexahouse@yandex.ru>
2022-01-05 15:59:21 -0500

    Change MAC address

--- Mac changer/Mac_changer.py
+++ Mac changer/Mac_changer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import subprocess
 import argparse
+import re
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description = "Changing MAC address")
@@ -16,10 +18,29 @@ def get_arguments():
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for {} to {}".format(interface, new_mac))
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.run(["ifconfig", interface, "down"])
+    subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.run(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.run(['ifconfig', interface], capture_output=True, text = True).stdout
+    mac_address_search_result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result[0]
+    else:
+        print('[-] Could not read MAC adress')
 
 args = get_arguments()
+
+initial_mac_addres = get_current_mac(args.interface)
+print('Initial MAC addres = {}'.format(initial_mac_addres))
+
 change_mac(args.interface, args.new_mac)
+
+new_mac_address = get_current_mac(args.interface)
+print('New MAC addres = {}'.format(new_mac_address))
+
+if new_mac_address == args.new_mac:
+    print('[+] MAC address was successfully change to {}'.format(new_mac_address))
+else:
+    print('[-] MAC address did not get changed')
