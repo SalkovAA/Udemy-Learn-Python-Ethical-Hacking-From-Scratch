bd2d50e9e8cffd0e9326557b2b6852d21dab60a6
SalkovAA <lexahouse@yandex.ru>
2022-01-06 09:01:57 -0500

    test

--- Network scanner/network_scanner.py
+++ Network scanner/network_scanner.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description = "Network scanner")
+    parser.add_argument( '--target', dest= 'target', type= str,  help= "Input target IP / IP range", required = True)
+    return parser.parse_args()
 
 def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list, unanswered = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)
+    clients_list = []
+    for element in answered_list:
+        clients_list.append({"ip" : element[1].psrc, "mac" : element[1].hwsrc})
+    return clients_list
+
+def print_result(result_list):
+    print("IP\t\t\tMac Address\n-----------------------------------------")
+    for client in result_list:
+        print('{}\t\t{}'.format(client['ip'], client['mac']))
     #scapy.arping(ip)
 
-scan("10.0.2.1/24")
+args = get_arguments()
+scan_result = scan(args.target)
+print_result(scan_result)
